fc8a3be36184701896b52531ed12de2fb1996202
Parth Singla <parthsingla2847@gmail.com>
2026-05-02 19:24:36 +0530

    Create ExpenseForm component with basic structure

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { useState } from "react";
 
-function ExpenseForm() {
+function ExpenseForm({ members, addExpense }) {
   return (
-    <div>ExpenseForm</div>
-  )
+    <form className="bg-white p-4 rounded-lg shadow-md mb-6">
+      <h2 className="text-xl font-bold mb-4">Add Expense</h2>
+    </form>
+  );
 }
 
-export default ExpenseForm
+export default ExpenseForm;
